e51cf1a289227a19b94dc58cbda8d31beba6bcbc
Berkay Terzioglu <terziogluberkay@gmail.com>
2021-02-20 21:43:06 +0300

    - Code optimization.

---
+++ src/main/java/tr/net/terzioglu/pfsak/UUEncode.java
@@ -0,0 +1,36 @@
+package tr.net.terzioglu.pfsak;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.mail.internet.MimeUtility;
+
+public class UUEncode implements Encode {
+
+    @Override
+    public byte[] decode(byte[] data) throws Exception {
+
+        InputStream inputStream = MimeUtility.decode(new ByteArrayInputStream(data), "uuencode");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Utility.copyStream(inputStream, baos);
+
+        return baos.toByteArray();
+
+    }
+
+    @Override
+    public byte[] encode(byte[] data) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        OutputStream outputStream = MimeUtility.encode(baos, "uuencode");
+        outputStream.write(data);
+        outputStream.flush();
+        outputStream.close();
+        return baos.toByteArray();
+
+    }
+
+}
